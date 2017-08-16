99e8f24281f294435929c976af991052af094886
Karol Niziałek <nizialek.karol@gmail.com>
2017-08-16 16:05:10 +0200

    Initial commit // Add Mongo db support and some initial parts of content tree handling

---
+++ CoreCms.Cms.ContentTreeHandler.Contract/IContentTreeHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CoreCms.Cms.Model.Content.ContentTree;
+
+namespace CoreCms.Cms.ContentTreeHandler.Contract
+{
+    public interface IContentTreeHandler
+    {
+        ContentNode GetContentNodeForUrl(string url);
+
+        string ResolveContentUrl(Guid contentNodeId);
+
+        void AddContentNode(Guid parentId, ContentNode newNode);
+    }
+}
---
+++ CoreCms.Cms.ContentTreeHandler/DefaultHierarchicContentTreeHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CoreCms.Cms.ContentTreeHandler.Contract;
+using CoreCms.Cms.Model.Content.ContentTree;
+using CoreCms.DataAccess.Contract;
+
+namespace CoreCms.Cms.ContentTreeHandler
+{
+    public class DefaultHierarchicContentTreeHandler : IContentTreeHandler
+    {
+        private readonly IRepository<ContentNode> _contentNodeRepository;
+
+        public DefaultHierarchicContentTreeHandler(IRepository<ContentNode> contentNodeRepository)
+        {
+            _contentNodeRepository = contentNodeRepository;
+        }
+        
+        public ContentNode GetContentNodeForUrl(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ResolveContentUrl(Guid contentNodeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddContentNode(Guid parentId, ContentNode newNode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
