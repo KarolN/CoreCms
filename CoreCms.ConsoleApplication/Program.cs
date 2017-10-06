4eff3a649d2c1efd7e80c1d2692c316c72c27881
Karol Niziałek <nizialek.karol@gmail.com>
2017-10-06 21:35:51 +0200

    Add images module, add image routing, loading and displaying

---
+++ CoreCms.ConsoleApplication/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreCms.Cms.Modules.Images.Model;
+using CoreCms.Cms.Modules.Pages.Model;
+using CoreCms.DataAccess.Base;
+using CoreCms.SampleSite.Pages;
+using MongoDB.Driver;
+
+namespace CoreCms.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("test");
+            var repo = new MongoRepository<ImagesTreeNode>(db);
+            var node = new ImagesDirectory{Name = "", Id = Guid.NewGuid(), Children = new List<ImagesTreeNode>
+            {
+                new ImagesDirectory()
+                {
+                    Name = "main",
+                    Id = Guid.NewGuid(),
+                    Children = new List<ImagesTreeNode>
+                    {
+                        new ImageNode
+                        {
+                            ImageId = Guid.NewGuid(),
+                            Id = Guid.NewGuid(),
+                            Name="img"
+                        }
+                    }
+                }
+            }};
+
+            repo.Insert(node);
+        }
+    }
+}
